79dad8e6f563928e5b2ec33e8d965ef67cf22627
GAMZE KURAL <gamzekural98@gmail.com>
2021-12-10 17:36:37 +0300

    Add project files.

---
+++ MCD_DataGridViewImage/urun.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_DataGridViewImage
+{
+    public class urun
+    {
+        public int Id { get; set; }
+        public string UrunTanim { get; set; }
+        public string ResimYol { get; set; }
+        public Image UrunResim
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(ResimYol))
+                {
+                    return Image.FromFile(ResimYol);
+                }
+                else
+                {
+                    return Image.FromFile(@"C:\Users\103AKSAM_GAMZE\Desktop\kitapResim\noImage.jpg");
+                }
+            }
+        }
+
+    }
+}
